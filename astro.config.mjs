80ecdcb704d5ec2a4c8cdd95a57f0d0b09636361
Andrew Kula <arkula931@gmail.com>
2025-09-07 09:11:51 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,6 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https:andrew-kula931.github.io',
-  base: '/profile-website/',
   trailingSlash: 'always',
   vite: {
     plugins: [tailwindcss()]
